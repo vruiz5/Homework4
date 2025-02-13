93ec335c509a88a027d4fd51ecdff5091575c39c
Victor Ruiz <vruiz5@stu.southtexascollege.edu>
2025-02-12 19:29:06 -0600

    Finished income expense component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
-  //import IncomeExpenses from './components/IncomeExpenses.vue';
-  //import TransactionList from "./components/TransactionList.vue";
-  //import AddTransaction from "./components/AddTransaction.vue";
-  import { ref, computed } from 'vue';
+  import IncomeExpenses from './components/IncomeExpenses.vue';
+  //import TransactionList from "./components/TransactionList.vue"
+  //import AddTransaction from "./components/AddTransaction.vue"
+  import { ref, computed } from 'vue'
 
-  const transcactions = ref([
+  const transactions = ref([
     {id: 1, text: 'Paycheck', amount: 700.00},
     {id: 2, text: 'Water Bill', amount: -72.83},
     {id: 3, text: 'Electric Bill', amount: -153.89},
@@ -14,7 +14,23 @@
   ])
 
   const sum = computed(() => {
-      return transcactions.value.reduce((acc, x) => {
+      return transactions.value.reduce((acc, x) => {
+          return acc + x.amount
+      }, 0)
+  })
+
+  const moneyIn = computed(() => {
+      return transactions.value
+      .filter((x) => x.amount > 0 )
+      .reduce((acc, x) => {
+          return acc + x.amount
+      }, 0)
+  }) 
+
+  const moneyOut = computed(() => {
+      return transactions.value
+      .filter((x) => x.amount < 0 )
+      .reduce((acc, x) => {
           return acc + x.amount
       }, 0)
   })
@@ -25,7 +41,7 @@
     <Header></Header>
     <div class="container">
         <Balance :total="sum"></Balance>
-        <IncomeExpenses></IncomeExpenses>
+        <IncomeExpenses :income="moneyIn" :expense="moneyOut"></IncomeExpenses>
         <TransactionList></TransactionList>
         <AddTransaction></AddTransaction>
     </div>
