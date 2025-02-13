542f41d83c3cee96e222e284e8d03a59b6fd8218
Victor Ruiz <vruiz5@stu.southtexascollege.edu>
2025-02-12 19:36:28 -0600

    Next

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-  import Header from './components/Header.vue';
-  import Balance from './components/Balance.vue';
-  import IncomeExpenses from './components/IncomeExpenses.vue';
+  import Header from './components/Header.vue'
+  import Balance from './components/Balance.vue'
+  import IncomeExpenses from './components/IncomeExpenses.vue'
   //import TransactionList from "./components/TransactionList.vue"
   //import AddTransaction from "./components/AddTransaction.vue"
   import { ref, computed } from 'vue'
